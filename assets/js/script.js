b84059d306cf1d2d2a97c6add0d931a2727048f5
Vivian <36191449+vivianegwu@users.noreply.github.com>
2019-03-31 00:15:46 +0100

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
 function showRegForm() {
-    var x = document.getElementById("loginBox");
-    if (x.style.display === "none") {
-      x.style.display = "fixed";
-    } else {
-      x.style.display = "none";
-    }
+    document.querySelector(".login-box").classList.remove("remove-form")
   }
